122238f3169dcfe56ec835a60072da6d6da02c48
ChickenLamb <vincentlin46@gmail.com>
2023-01-06 03:28:37 +0800

    ignore -f dist/

---
+++ dist/server/assets/about.page.2a0395aa.js
@@ -0,0 +1,16 @@
+import { j as jsxs, F as Fragment, a as jsx } from "./chunk-746fc63d.js";
+import "react/jsx-runtime";
+const code = "";
+function Page() {
+  return /* @__PURE__ */ jsxs(Fragment, { children: [
+    /* @__PURE__ */ jsx("h1", { children: "About" }),
+    /* @__PURE__ */ jsxs("p", { children: [
+      "Demo using ",
+      /* @__PURE__ */ jsx("code", { children: "vite-plugin-ssr" }),
+      "."
+    ] })
+  ] });
+}
+export {
+  Page
+};
---
+++ dist/server/pageFiles.js
@@ -0,0 +1,64 @@
+const exportNames$5 = ["Page"];
+const __vite_glob_1_0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  exportNames: exportNames$5
+}, Symbol.toStringTag, { value: "Module" }));
+const exportNames$4 = ["Page"];
+const __vite_glob_1_1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  exportNames: exportNames$4
+}, Symbol.toStringTag, { value: "Module" }));
+const exportNames$3 = ["Page"];
+const __vite_glob_1_2 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  exportNames: exportNames$3
+}, Symbol.toStringTag, { value: "Module" }));
+const exportNames$2 = ["Page"];
+const __vite_glob_1_3 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  exportNames: exportNames$2
+}, Symbol.toStringTag, { value: "Module" }));
+const exportNames$1 = ["render", "passToClient"];
+const __vite_glob_3_0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  exportNames: exportNames$1
+}, Symbol.toStringTag, { value: "Module" }));
+const exportNames = ["render"];
+const __vite_glob_5_0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  exportNames
+}, Symbol.toStringTag, { value: "Module" }));
+const pageFilesLazy = {};
+const pageFilesEager = {};
+const pageFilesExportNamesLazy = {};
+const pageFilesExportNamesEager = {};
+const pageFilesList = [];
+const neverLoaded = {};
+const isGeneratedFile = true;
+const pageFilesLazyIsomorph1 = /* @__PURE__ */ Object.assign({ "/pages/about/index.page.tsx": () => import("./assets/about.page.2a0395aa.js"), "/pages/assets/index.page.tsx": () => import("./assets/assets.page.eeb9c8fd.js"), "/pages/index/index.page.tsx": () => import("./assets/index.page.9f2f757e.js"), "/renderer/_error.page.tsx": () => import("./assets/_error.page.4fa6da32.js") });
+const pageFilesLazyIsomorph = { ...pageFilesLazyIsomorph1 };
+pageFilesLazy[".page"] = pageFilesLazyIsomorph;
+const pageFilesExportNamesEagerIsomorph1 = /* @__PURE__ */ Object.assign({ "/pages/about/index.page.tsx": __vite_glob_1_0, "/pages/assets/index.page.tsx": __vite_glob_1_1, "/pages/index/index.page.tsx": __vite_glob_1_2, "/renderer/_error.page.tsx": __vite_glob_1_3 });
+const pageFilesExportNamesEagerIsomorph = { ...pageFilesExportNamesEagerIsomorph1 };
+pageFilesExportNamesEager[".page"] = pageFilesExportNamesEagerIsomorph;
+const pageFilesLazyServer1 = /* @__PURE__ */ Object.assign({ "/renderer/_default.page.server.tsx": () => import("./assets/_default.page.server.d6254314.js") });
+const pageFilesLazyServer = { ...pageFilesLazyServer1 };
+pageFilesLazy[".page.server"] = pageFilesLazyServer;
+const pageFilesExportNamesEagerServer1 = /* @__PURE__ */ Object.assign({ "/renderer/_default.page.server.tsx": __vite_glob_3_0 });
+const pageFilesExportNamesEagerServer = { ...pageFilesExportNamesEagerServer1 };
+pageFilesExportNamesEager[".page.server"] = pageFilesExportNamesEagerServer;
+const pageFilesEagerRoute1 = /* @__PURE__ */ Object.assign({});
+const pageFilesEagerRoute = { ...pageFilesEagerRoute1 };
+pageFilesEager[".page.route"] = pageFilesEagerRoute;
+const pageFilesExportNamesEagerClient1 = /* @__PURE__ */ Object.assign({ "/renderer/_default.page.client.tsx": __vite_glob_5_0 });
+const pageFilesExportNamesEagerClient = { ...pageFilesExportNamesEagerClient1 };
+pageFilesExportNamesEager[".page.client"] = pageFilesExportNamesEagerClient;
+export {
+  isGeneratedFile,
+  neverLoaded,
+  pageFilesEager,
+  pageFilesExportNamesEager,
+  pageFilesExportNamesLazy,
+  pageFilesLazy,
+  pageFilesList
+};
