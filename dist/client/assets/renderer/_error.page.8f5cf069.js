f2edaf558cc21fb4412990faa1b70ff6b3da308e
ChickenLamb <vincentlin46@gmail.com>
2023-01-07 04:39:45 +0800

    style fix

--- dist/client/assets/renderer/_error.page.8f5cf069.js
+++
@@ -1,1 +0,0 @@
-import{b as r,F as n,j as e}from"../chunk-e6bd3ea7.js";function t({is404:o}){return o?r(n,{children:[e("h1",{children:"404 Page Not Found"}),e("p",{children:"This page could not be found."})]}):r(n,{children:[e("h1",{children:"500 Internal Server Error"}),e("p",{children:"Something went wrong."})]})}export{t as Page};
