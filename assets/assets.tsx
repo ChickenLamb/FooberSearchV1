398b8a08ebf856a9696bca3f6e2b0cab76b2398b
ChickenLamb <vincentlin46@gmail.com>
2023-01-07 04:31:03 +0800

    add footer and styling

--- assets/assets.tsx
+++ assets/assets.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import logo from "/assets/logo.svg";
-import styled from "styled-components";
-
 export function ColorPalette() {
   return (
     <div
@@ -37,19 +35,19 @@ export function ColorPalette() {
         #FFB6C1 (light pink)
       </div>
       <div style={{ backgroundColor: "#FF8C00", flexBasis: "25%" }}>
-        #FF8C00 (dark orange)*
+        #FF8C00 (dark orange)<strong>*primary-color</strong>
       </div>
       <div style={{ backgroundColor: "#FFC0CB", flexBasis: "25%" }}>
-        #FFC0CB (pink)
+        #FFC0CB (pink)<strong>***shadow-color</strong>
       </div>
       <div style={{ backgroundColor: "#FF4500", flexBasis: "25%" }}>
-        #FF4500 (orangered)
+        #FF4500 (orangered)******footer-color
       </div>
       <div style={{ backgroundColor: "#FF69B4", flexBasis: "25%" }}>
         #FF69B4 (hot pink)
       </div>
       <div style={{ backgroundColor: "#FF1493", flexBasis: "25%" }}>
-        #FF1493 (deep pink)
+        #FF1493 (deep pink)<strong>**secondary-color</strong>
       </div>
       <h3 style={{ flexBasis: "100%" }}>motion color:</h3>
       <div style={{ backgroundColor: "#00FF00", flexBasis: "25%" }}>
@@ -92,6 +90,13 @@ export function ColorPalette() {
       >
         #2F2F4F
       </div>
+      <h3 style={{ flexBasis: "100%" }}>other color:</h3>
+      <div style={{ backgroundColor: "#FF0000", flexBasis: "25%" }}>
+        #FF0000 (red)<strong>****link-color</strong>
+      </div>
+      <div style={{ backgroundColor: "#FFFFFF", flexBasis: "25%", border: "1px solid black" }}>
+        #FFFFFF (red)<strong>*****text-hover-color</strong>
+      </div>
     </div>
   );
 }
@@ -120,17 +125,39 @@ export function Fonts() {
       <p style={{ flexBasis: "100%" }}>The cat sat on the mat.</p>
       <h3 style={{ flexBasis: "100%" }}>中文（繁體）:</h3>
       <p style={{ flexBasis: "100%" }}>花开富贵在君王邸，花谢贫贱在我床前。</p>
-      <cite><strong>font-weight</strong>:100,300,<strong>400(default)</strong>,500,700,900.<br/> html default <strong>font-size</strong>: <strong>10pt</strong></cite>
+      <cite><strong>font-weight</strong>:100,300,<strong>400(default)</strong>,500,700,900.<br /> html default <strong>font-size</strong>: <strong>10pt</strong></cite>
     </div>
   );
 }
 export function Logo() {
   return (
-    <
-    >
-      <a href="/">
-        <img style={{display:"block"}} src={logo} height={'auto'} width={"100%"} alt="logo" />
+    <>
+      <a id="logo" href="/">
+        <img style={{ display: "block" }} src={logo} height={'auto'} width={"100%"} alt="logo" />
       </a>
+    </>
+  );
+}
+
+export function Footer() {
+  return (
+    <>
+
+      <div
+        style={{
+          width: "95%",
+          height: "fit-content",
+          color: "var(--white-color)",
+          backgroundColor: "var(--footer-color)",
+          padding: "30px 0px 30px 5%",
+        }}
+      ><span>Footer</span>
+        
+        <a style={{margin:"0 10px"}} href="#about">About</a>
+        <a style={{margin:"0 10px"}} href="#advertising">Advertising</a>
+        <a style={{margin:"0 10px"}} href="#business">Business</a>
+        <a style={{margin:"0 10px"}} href="#how-search-works">How Search works</a>
+      </div>
     </>
   );
 }
@@ -138,25 +165,22 @@ export function Logo() {
 export function ComponentsWithoutChildren() {
   return (
     <div
-    style={{
-      width:"fit-content",
-      height:"fit-content"
-    }}
+      style={{
+        width: "fit-content",
+        height: "fit-content"
+      }}
     >
       Components Without Children
     </div>
   );
 }
-const ComponentWithChildren = MyStyled.div`
-  width:fit-content;
-  height:fit-content;
-`;
+
 export function ComponentsWithChildren({ children }: { children: React.ReactNode }) {
   return (
     <div
       style={{
-        width:"fit-content",
-        height:"fit-content"
+        width: "fit-content",
+        height: "fit-content"
       }}
     >
       {children}
@@ -167,17 +191,18 @@ export function ComponentsWithChildren({ children }: { children: React.ReactNode
 // show all assets done
 export function Assets() {
   return (
-    <div style={{margin:"10px"}}>
+    <div style={{ margin: "10px" }}>
+      {/* <h3>Build Component Using Options below:</h3>
+      <ComponentsWithoutChildren/>
+      <ComponentsWithChildren>Components With Children</ComponentsWithChildren> */}
       <ColorPalette />
       <Fonts />
-
-      <h3>Build Component Using Options below:</h3>
-      <ComponentsWithoutChildren/>
-      <ComponentWithChildren>Component With Children</ComponentWithChildren>
-      <ComponentsWithChildren>Components With Children</ComponentsWithChildren>
-      <div style={{width:"500px"}}><Logo /></div>
-      
+      <div style={{ width: "500px" }}><Logo /></div>
+      <hr />
+      <button>this is a button</button>
       <input type={"text"}></input>
+      <hr />
+      <Footer />
     </div>
   );
 }
